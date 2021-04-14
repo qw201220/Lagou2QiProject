49f98a1f50c4f8011ee28faa1952b3d333e24929
qw201220 <z717073296@163.com>
2021-04-15 00:45:12 +0800

    python第一次课程

--- pythoncode/demo.py
+++
@@ -1,9 +0,0 @@
-name = "tone"
-
-def func():
-    # global name
-    name = "tome"
-    print(f"name = {name}")
-print(f"name = {name}")
-func()
-print(f"name = {name}")
--- pythoncode/main.py
+++ pythoncode/main.py
@@ -1,20 +1,26 @@
 have_girl = False
-def send():
-    print("发女朋友啦。。")
-    global have_girl
-    have_girl = True
-    print(f"have_girl = {have_girl}")
+def noParamMethod1():
+    print("这是一个无返回值的无参方法")
+def noParamMethod2():
+    print("这是一个有返回值的无参方法")
+    return "这是一个有返回值的无参方法"
 
+def paramMethod1(param):
+    print("这是一个无返回值的有参方法: %s" %param)
 
-def show():
-    if have_girl == True:
-        print("有女朋友，好开心~~")
-    else:
-        print("单身贵族 *__*")
+def paramMethod2(param):
+    print("这是一个有返回值的有参方法: %s" %param)
+    return param
+
 
 
 
 if __name__ == '__main__':
     print(__name__)
-    send()
-    show()
+    print(noParamMethod1())
+    print("..............................................")
+    print(noParamMethod2())
+    print("..............................................")
+    print(paramMethod1("有参无返回值"))
+    print("..............................................")
+    print(paramMethod2("有参有返回值"))
